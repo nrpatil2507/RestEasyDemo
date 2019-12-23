730c44ecd770fc38269d1f1f1d6d3c14696e0a1e
nrpatil2507 <nutanpatil2507@gmail.com>
2019-12-23 18:55:05 +0530

    initial commit

---
+++ src/main/java/com/service/MessageApplication.java
@@ -0,0 +1,19 @@
+package com.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class MessageApplication extends Application {
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public MessageApplication() {
+		singletons.add(new MessageRestService());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
---
+++ src/main/java/demo/demo.java
@@ -0,0 +1,8 @@
+package demo;
+
+class demo {
+	public static void main(String args[]) {
+		System.out.println("hello gradle");
+
+	}
+}
